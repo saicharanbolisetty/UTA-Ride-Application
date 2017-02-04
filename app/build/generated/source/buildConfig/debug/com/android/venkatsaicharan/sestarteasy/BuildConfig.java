e6dbb9d0101f79ba36ca7915e9f23ac3eae46882
saicharanbolisetty <saicharanbolisetty@icloud.com>
2017-02-04 16:48:49 -0600

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/android/venkatsaicharan/sestarteasy/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.android.venkatsaicharan.sestarteasy;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.android.venkatsaicharan.sestarteasy";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
